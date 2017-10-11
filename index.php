afaf5d1d27095e9c7c20b522c72a0e2dfaf0c2d4
shaheershukur <shaheershukur@gmail.com>
2017-10-11 19:13:36 +0530

    update

--- index.php
+++ index.php
@@ -2,16 +2,29 @@
 
     <head>
         <title>Xdb</title>
-        <link rel="stylesheet" type="text/css" href="style/style.css">
+        <link rel="stylesheet" type="text/css" href="style/login-style.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"/>
     </head>
 
     <body>
-        <div class="headerDiv">
-            xdb
-        </div>
+       <div class="body-style">
+            <div class="header-div">
 
-        <div class="loginBox">
+            </div>
 
+            <div>
+                <div class="login-box">
+                    <div class="div-title">
+                        Welcome
+                    </div>
+
+                    <form class="input-field">
+                        <input type="text" placeholder="Login ID">
+                        <input type="password" placeholder="Password">
+                    </form>
+
+                </div>
+            </div>
         </div>
     </body>
 
