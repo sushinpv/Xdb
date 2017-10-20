904b7eeb157c1a62bddc379056c27e96a2aaa5cc
shaheershukur <shaheershukur@gmail.com>
2017-10-21 00:17:57 +0530

    update

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
-    require("pages/connect.php");
+    include("pages/config.php");
+    session_start();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $user = mysqli_real_escape_string($connect,$_POST['username']);
+        $passwd = mysqli_real_escape_string($connect,$_POST['passwd']);
+
+        $sql = "SELECT * FROM xdb WHERE usn='$user' AND password='$passwd'";
+        $result = mysqli_query($connect,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        #$active = $row['active'];
+
+        $count = mysqli_num_rows($result);
+
+        if($count == 1){
+            session_register("myusername");
+            $_SESSION['login_user'] = $user;
+            header("location: welcome.php");
+        }else{
+            $error = "Invalid credentials!";
+        }
+    }
 ?>
 
 <html>
@@ -16,13 +37,13 @@
                 Xdb
         </p>
 
-        <form action="index.php" method="post">
+        <form action="" method="post">
 
           <div class="container">
 
-            <input type="text" placeholder="Login ID" name="username"/>
+            <input type="text" placeholder="Login ID" name="username" maxlength="10">
 
-            <input type="password" placeholder="Password" name="passwd"/>
+            <input type="password" placeholder="Password" name="passwd">
 
             <button type="submit">Login</button>
           </div>
--- pages/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $connection = mysqli_connect("localhost","root","") or die(mysqli_error());
-    mysqli_select_db($connection,"xdb") or die(mysqli_error());
-?>
